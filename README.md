1771320774c856de7e8f4e9035f41ffb31d5ff97
kalanaelapatha <38855998+kalanaelapatha@users.noreply.github.com>
2019-12-26 12:26:07 +0530

    create route
    Created the basic routes with Dummy Data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Go_Rest_API
 This is the rest-api which include simple CRUD
+
+after clone that put that in to user go Path then src folder
+go -> src -> github.com ->[github username]-> cloned folder
---
+++ go_rest_api/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//we use json request and responce and http protocols so that we must need to import those kind of packages to that projects
+//we use to math lib to generate a random number when we use the Id
+//and we need to use String Convert to convert the json to string
+
+//Books Struct(Model)
+type Books struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Tilte  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var books []Books //use that to mock data first -> 1 st Step
+
+// Get All books
+
+func getBooks(w http.ResponseWriter, route *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json") // third Step
+	json.NewEncoder(w).Encode(books)                   // third Step
+
+}
+
+//Get Single Book by id
+func getBook(w http.ResponseWriter, route *http.Request) {
+
+}
+
+// Create a book
+func createBook(w http.ResponseWriter, route *http.Request) {
+
+}
+
+//update a book
+func updateBook(w http.ResponseWriter, route *http.Request) {
+
+}
+
+//delete a book
+func deleteBook(w http.ResponseWriter, route *http.Request) {
+
+}
+
+func main() {
+	//init  Router
+
+	route := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+
+	books = append(books, Books{ID: "1", Isbn: "234234", Tilte: "X Files", Author: &Author{FirstName: "Steve", LastName: "Harvey"}}) // second step
+	books = append(books, Books{ID: "2", Isbn: "456567", Tilte: "Chemist", Author: &Author{FirstName: "jhone", LastName: "Doe"}})    // second step
+	// Route Handlers / End Points
+
+	route.HandleFunc("/api/books", getBooks).Methods("GET")
+	route.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	route.HandleFunc("/api/books", createBook).Methods("POST")
+	route.HandleFunc("/api/book/{id}", updateBook).Methods("PUT")
+	route.HandleFunc("/api/books", deleteBook).Methods("DELETE")
+
+	//setup the server
+
+	log.Fatal(http.ListenAndServe(":8000", route))
+
+}
